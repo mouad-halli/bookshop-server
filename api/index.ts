b01c90fcb8f164c8671b58908db040ecfc26fea2
mouad halli <mouadhalli1337@gmail.com>
2025-03-25 02:25:25 +0000

    updated cors config

--- api/index.ts
+++ api/index.ts
@@ -22,13 +22,10 @@ const { INTERNAL_SERVER_ERROR } = STATUS_CODES
 
 const app = express()
 
-// app.use(cors({
-// 	origin: [CLIENT_URL],
-// 	credentials: true,
-// }))
 app.use(cors({
-	origin: "*",
-	credentials: false,
+	origin: [CLIENT_URL],
+	credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }))
 
 app.use(cookieParser())
@@ -49,8 +46,6 @@ app.use('/order', orderRoutes)
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World')
 })
-
-// app.options("*", cors({ origin: [CLIENT_URL], credentials: true }));
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 	const errorStatus = error.status || 500
