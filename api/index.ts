ec15108aceff8614710f0cf4419157ff2f2bd9f8
mouad halli <mouadhalli1337@gmail.com>
2025-03-25 02:33:06 +0000

    save

--- api/index.ts
+++ api/index.ts
@@ -25,7 +25,7 @@ const app = express()
 app.use(cors({
 	origin: [CLIENT_URL],
 	credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
 }))
 
 app.use(cookieParser())
