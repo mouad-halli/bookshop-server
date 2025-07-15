a3e6fac51493ace921df9046568b8c59bbabffc2
mouad halli <mouadhalli1337@gmail.com>
2025-07-15 08:59:34 +0100

    updated mongoose connection options

--- api/config/database.ts
+++ api/config/database.ts
@@ -5,7 +5,11 @@ mongoose.set("strictQuery", false)
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(String(DB_LINK))
+        await mongoose.connect(String(DB_LINK), {
+            bufferCommands: false, // Disable command buffering (models won't queue commands if not connected)
+            serverSelectionTimeoutMS: 30000, // Wait up to 30s for MongoDB to respond
+            connectTimeoutMS: 30000,         // Wait up to 30s for initial TCP connection
+    })
     } catch (error) {
         throw error
     }
